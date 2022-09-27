957cadd75dd7490f534a41973da1d19fe33799c9
3x071c <dev@3x071c.com>
2022-09-27 02:00:07 +0200

    Added some Astro integrations & css

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,20 @@
+import partytown from "@astrojs/partytown";
+import compressor from "astro-compressor";
+import htmlMinifier from "astro-html-minifier";
+import { astroImageTools } from "astro-imagetools";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [
+		astroImageTools,
+		partytown({}),
+		htmlMinifier(),
+		compressor(),
+	],
+	vite: {
+		ssr: {
+			external: ["svgo"], // for the astro-icon tool
+		},
+	},
+});
